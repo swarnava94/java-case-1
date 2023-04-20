56a6cace1b7f1e91350180c896d7149898f7e1b0
Swarnava Majumder <mailswarnava@gmail.com>
2023-04-20 19:21:28 +0530

    first commit

---
+++ src/main/java/com/theopentutorials/ejb3/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.theopentutorials.ejb3;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+public class HelloWorldBean implements HelloWorld {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
